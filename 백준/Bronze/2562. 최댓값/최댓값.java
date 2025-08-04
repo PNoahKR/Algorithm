4d748a7a2c9353cf0085f66afa46d21551d3b65f
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-08-05 00:54:03 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int max = 0;
+        int num = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] > max) {
+                max = arr[i];
+                num = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(num);
+    }
+}
