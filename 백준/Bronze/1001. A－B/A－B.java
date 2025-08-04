cf588931af1ef6891180ef06b44a595f7864db12
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-08-05 00:56:45 +0900

    [Bronze V] Title: A-B, Time: 180 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.print(A - B);
+    }
+}
