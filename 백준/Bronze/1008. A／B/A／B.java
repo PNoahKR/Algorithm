60fcdec88bb1477851a3fa2e03a5f8ce361ec5b4
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-08-05 00:57:27 +0900

    [Bronze V] Title: A/B, Time: 188 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        Double A = in.nextDouble();
+        Double B = in.nextDouble();
+        System.out.print(A / B);
+    }
+}
