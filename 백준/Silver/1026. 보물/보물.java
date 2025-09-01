823057e582d25481ff4f308e1fdcd8282f6a39b2
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-09-01 22:56:44 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 정수 배열의 길이 N
+        int N = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // B는 재배열 하면 안된다... 지만 사실 최솟값만 구하면 되는 문자라서
+        // A 오름차순 정렬, B는 내림차순 정렬 후 계산하면 된다.
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += Math.multiplyExact(A[i], B[i]);
+        }
+        System.out.println(sum);
+    }
+}
