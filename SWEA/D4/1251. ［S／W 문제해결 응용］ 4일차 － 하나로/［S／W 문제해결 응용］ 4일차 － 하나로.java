85b5af9d9c1329a8f4c1827236dc7ce2c1f6039d
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-09-11 16:24:57 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 822 ms, Memory: 97,260 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,27 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	// 간선 클래스
-	static class Edge implements Comparable<Edge> {
-		int from;
-		int to;
-		// 다음 섬까지의 거리
-		double distance;
 
-		public Edge(int x, int y, double l) {
-			this.from = x;
-			this.to = y;
-			this.distance = l;
+	static class Edge implements Comparable<Edge> {
+		int to;
+		double dist;
+
+		public Edge(int to, double dist) {
+			this.to = to;
+			this.dist = dist;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-			return (int) (this.distance - o.distance);
+			return Double.compare(this.dist, o.dist);
 		}
 	}
 
@@ -35,7 +32,11 @@ public class Solution {
 			int N = Integer.parseInt(br.readLine());
 
 			// 간선리스트 생성
-			List<Edge> edges = new ArrayList<>();
+			List<Edge>[] edges = new ArrayList[N];
+			for (int i = 0; i < N; i++) {
+				edges[i] = new ArrayList<>();
+			}
+
 
 			// 섬의 X좌표 / Y좌표 받을 배열
 			int[] xPosition = new int[N];
@@ -64,34 +65,39 @@ public class Solution {
 					long p = xPosition[i] - xPosition[j];
 					long q = yPosition[i] - yPosition[j];
 					double distance = (double)(p * p + q * q);
-					edges.add(new Edge(i, j, distance));
+					edges[i].add(new Edge(j, distance));
+					edges[j].add(new Edge(i, distance));
 				}
 			}
-
-			// Krusakal
-			// 클래스에서 정리할 대로 정렬
-			Collections.sort(edges);
-
-			// p 배열 생성 후 초기화
-			int[] p = new int[N];
-			for (int i = 0; i < N; i++) {
-				p[i] = i;
-			}
-			// 총길이
+			
 			double totalDistance = 0;
-			// 간선 수 체크
-			int edgeCount = 0;
-			// 가장 작은 거리의 간선부터
-			for (Edge edge : edges) {
-				if (findSet(p, edge.from) != findSet(p, edge.to)) {
-					union(p, edge.from, edge.to);
-					totalDistance += edge.distance;
+			
+			// PriorityQueue / boolean 배열 생성
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			boolean[] visited = new boolean[N];
+			
+			pq.offer(new Edge(0, 0));
+			
+			while(!pq.isEmpty()) {
+				Edge curEdge = pq.poll();
+				
+				int next = curEdge.to;
+				double dist = curEdge.dist;
+				
+				if(visited[next]) continue;
+				
+				visited[next] = true;
+				totalDistance += dist;
+				
+				for (Edge e : edges[next]) {
+					int nnext = e.to;
+					double ndist = e.dist;
+					if(!visited[nnext]) {
+						pq.offer(new Edge(nnext, ndist));
+					}
 				}
-
-				// 간선이 모두 선택되면 탈출
-				if (edgeCount == N - 1)
-					break;
 			}
+			
 			// 세율
 			double E = Double.parseDouble(br.readLine());
 			totalDistance *= E;
