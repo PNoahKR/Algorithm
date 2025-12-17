ccee8ab9f865ba64c70c37d2d3274aade7b7e3f2
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2025-12-17 16:00:59 +0900

    [D2] Title: 파리 퇴치, Time: 90 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,51 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
-import java.io.FileInputStream;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            // 입력
+            // N: 영역의 크기 M: 파리채의 크기
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
+            int[][] space = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    space[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-	
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			// 입력
-			// 파리영역 크기
-			int N = sc.nextInt();
-			// 파리채 크기
-			int M = sc.nextInt();
+            // 로직
+            // 잡은 파리의 최대 마리 수
+            int max  = 0;
 
-			int[][] arr = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					arr[i][j] = sc.nextInt();
-				}
-			}
+            for (int i = 0; i <= N - M; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    int sum = 0;
+                    for (int r = i; r < i + M; r++) {
+                        for (int c = j; c < j + M; c++) {
+                            sum += space[r][c];
+                        }
+                    }
+                    max = Math.max(max, sum);
+                }
+            }
 
-			// 로직
-			// 잡은 최대 파리수
-			int max = 0;
-			// 파리 영역 에서 파리채 만큼만 잡기
-			// for i = 0, i < N-M+1
-			// for j = 0, j < N-M+1
-			for (int i = 0; i < N - M + 1; i++) {
-				for (int j = 0; j < N - M + 1; j++) {
-					int sum = 0; // 현재 위치에서 잡은 파리의 수
-					for (int r = 0; r < M; r++) {
-						for (int c = 0; c < M; c++) {
-							sum += arr[i+r][j+c];
-						}
-					}
-					max = Math.max(max, sum);
-				}
-			}
+            // 출력
+            System.out.println("#" + test_case + " " + max);
 
-			// 출력
-			System.out.println("#" + test_case + " " + max);
-		}
-	}
+        } // tc반복문
+    }
 }
