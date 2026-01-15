aae596e36dfd3756c74c5acf4ab058e446e996e2
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-01-15 09:07:12 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 23004 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        int lastIndex = 0;
+        int sum = 0;
+
+        for(int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sum -= list.get(--lastIndex);
+                list.remove(lastIndex);
+            }
+            else {
+                list.add(num);
+                sum += num;
+                lastIndex++;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
