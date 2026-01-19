f892b2c03301872c273566935bfb15755424458b
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-01-19 11:32:20 +0900

    [Gold V] Title: 회의실 배정, Time: 512 ms, Memory: 43868 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<int[]> meetings = new ArrayList<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            meetings.add(new int[]{start, end});
+        }
+
+        meetings.sort((a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+
+        int count = 0;
+        int lastEndTime = 0;
+
+        for (int[] meeting : meetings) {
+            if (meeting[0] >= lastEndTime) {
+                count++;
+                lastEndTime = meeting[1];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
