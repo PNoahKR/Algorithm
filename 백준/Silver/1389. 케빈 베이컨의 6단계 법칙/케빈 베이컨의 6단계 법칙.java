8d14fc35e59b5510b19fd21e6ab3ad9e79a89d0c
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-01-27 22:31:38 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 108 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 987654321; // 초기화용
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
+
+        int[][] dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 양방향 친구 관계
+            dist[a][b] = dist[b][a] = 1;
+        }
+
+        // 플로이드-워셜
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        int minBacon = Integer.MAX_VALUE;
+        int resultIdx = -1;
+
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += dist[i][j];
+            }
+
+            if (sum < minBacon) {
+                minBacon = sum;
+                resultIdx = i;
+            }
+        }
+
+        System.out.println(resultIdx);
+    }
+}
