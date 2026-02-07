eef90119b1fe977ba2de49d58de59b677b0e10dd
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-02-08 01:34:54 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -4,10 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		System.out.println(a - b);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()));
     }
 }
