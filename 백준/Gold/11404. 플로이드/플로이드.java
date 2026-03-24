e35d6f6c0a3e345e37f24611c769452e442de410
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-03-24 21:29:01 +0900

    [Gold IV] Title: 플로이드, Time: 408 ms, Memory: 45456 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 10000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n+1][n+1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i != j) {
+                    arr[i][j] = MAX;
+                }
+            }
+        }
+
+        StringTokenizer st;
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    // 최단경로 초기화
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 갈 수 없는 곳은 0으로 초기화
+                if (arr[i][j] == MAX) {
+                    arr[i][j] = 0;
+                }
+ 
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
