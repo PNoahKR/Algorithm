07b481d98a057b4ec57dbd20e661cce655312ea0
Jiwoo Park <156992925+PNoahKR@users.noreply.github.com>
2026-03-29 23:33:30 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        char[] arr = str.toCharArray();
+        
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        
+        System.out.println(sb);
+    }
+}
